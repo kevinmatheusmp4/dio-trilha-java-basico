cf3852c00c17e769e3549325ea0e03fc3129ce2a
kevin <kevinmatheusquaresma@gmail.com>
2025-06-29 17:57:19 -0300

    mias um teste

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,5 @@
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Olá, Mundo!");
+        System.out.println("Olá, Mundo !");
     }
 }
